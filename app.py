a3c8ac8f9267b2ecad4c03576870a3b1511b5a30
imamahnf <104013142+imamahnf@users.noreply.github.com>
2024-12-20 02:04:54 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import joblib
 
 # Load the trained model
-model_path = 'sentiment_analysis_model.pkl'
+model_path = 'deployment_pipeline.pkl'
 model = joblib.load(model_path)
 
 # Define prediction function
